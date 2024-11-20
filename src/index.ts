3f77d8e21a2c26b247359a3af451a4f87be8ac52
foxt <me@foxt.dev>
2024-11-20 00:58:39 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import http from 'http';
+import { MongoClient } from 'mongodb';
+import { Readable } from 'stream';
+import { parseInformPacket } from "./inform/informpacket.js";
+
+let mongo = new MongoClient("mongodb://localhost:27017");
+let db = mongo.db('unispi');
+let collections = db.listCollections().toArray();
+let transactions = collections.then(async (collections) =>
+    collections.find((a) => a.name == 'txns') ||
+    await db.createCollection('txns', { timeseries: { timeField: "timestamp", metaField: "meta", granularity: "seconds" } })
+).then(() => db.collection('txns'));
+
+
+let keys: Record<string, string> = {};
+if (!existsSync('keys.txt')) 
+    writeFileSync('keys.txt', `# This file is used to store the keys used to decrypt the inform packets. One per line.\n# Example: {"mac": "aa:bb:cc:dd:ee:ff", "x_authkey": "ba86f2bbe107c7c57eb5f2690775c712"}\n`);
+try {
+    let lines = readFileSync('keys.txt').toString().split('\n');
+    for (let line of lines) {
+        if (line.startsWith("#")) continue;
+        if (!line.startsWith("{")) {
+            console.warn("Invalid line in keys.txt, ignoring", line);
+            continue;
+        }
+        let {mac, x_authkey: key} = JSON.parse(line.replace(/ObjectId\(([^)]+)\)/g, '$1 '));
+        if (typeof mac !== 'string' || typeof key !== 'string') {
+            console.warn("Invalid key in keys.txt, ignoring", line);
+            continue;
+        }
+        keys[mac.toLocaleLowerCase()] = key;
+    }   
+} catch(e) {
+    console.error("Failed to load keys.json, ignoring",e )
+}
+console.log("Loaded keys", keys);
+
+
+
+
+async function tryDumpReqRes(ip: string, reqBody:Buffer, resBody:Buffer) {
+    try {
+        let req = await parseInformPacket(reqBody, (mac) => keys[mac]);
+        if (req.error) throw req.error;
+
+        let res = await parseInformPacket(resBody, (mac) => keys[mac]);
+        if (res.error) throw res.error;
+
+        (await transactions).insertOne({
+            timestamp: new Date(),
+            meta: {
+                ip: ip,
+                mac: req.head.mac,
+            },
+            req: {
+                head: req.head,
+                payload: req.data
+            },
+            res: {
+                head: res.head,
+                payload: res.data
+            }
+        })
+    } catch(e) {
+        console.error(e)
+    }
+}
+
+function readToEnd(read: Readable) {
+    return new Promise<Buffer>((resolve, reject) => {
+        let data = [];
+        read.on('data', (chunk) => {
+            data.push(chunk);
+        });
+        read.on('end', () => {
+            resolve(Buffer.concat(data));
+        });
+        read.on('error', reject);
+    });
+}
+
+
+
+
+const server = http.createServer(async (req, res) => {
+    console.log('Request received from ' + req.connection.remoteAddress);
+    console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
+    for (let header in req.headers) {
+        console.log(`${header}: ${req.headers[header]}`);
+    }
+    console.log(req.url);
+    let body = await readToEnd(req);
+
+
+    let proxiedReq = http.request({
+        hostname: '192.168.2.11',
+        port: 8080,
+        path: '/inform',
+        method: 'POST',
+        headers: req.headers
+    })
+    proxiedReq.write(body);
+    proxiedReq.end();
+    proxiedReq.on('response', async (response) => {
+        res.writeHead(response.statusCode, response.headers);
+        let data = await readToEnd(response);
+        tryDumpReqRes(req.socket.remoteAddress,body, data);
+
+        //writeFileSync('response-' + Date.now() + '.bin', data);
+        res.write(data);
+        res.end();
+    });
+    
+});
+server.listen(8080);
---
+++ src/inform/informpacket.ts
@@ -0,0 +1,98 @@
+import { subtle, webcrypto } from 'crypto';
+import { uncompress as unSnappy } from 'snappyjs';
+import { inflateSync as inflateZlib } from 'zlib';
+import { InformPacketFlag, InformPacketHeader } from '../types/InformPacket';
+
+// Default key for UBNT devices - literally just `md5("ubnt")`
+const UBNT_DEFAULT_KEY = "ba86f2bbe107c7c57eb5f2690775c712";
+
+function readInformHeader(packet: Buffer) {
+    var header = packet.subarray(0,40)
+    // check magic is 'TNBU' (trivia: UBNT backwards!)
+    if (header.readUint32BE(0) !== 1414414933) 
+       throw new Error("expected TNBU (1414414933), got " + [...packet.subarray(0,4)].map(a => String.fromCharCode(a)).join("") + " (" + header.readUint32BE(0) + ")");
+    
+    let payloadLength = header.readUint32BE(36)
+    if (payloadLength > packet.length - 40) throw new Error("Payload is too short, expected " + payloadLength + " got " + (packet.length - 40) + " bytes.")
+    let payload = packet.subarray(40, payloadLength + 40)
+
+    let flags = header
+        .readUint16BE(14)
+        .toString(2)
+        .split('')
+        .reverse()
+        .map((v,x) => 
+            (v == "1" ? (
+                x == 0 ? "Encrypted" :
+                x == 1 ? "Compressed" :
+                x == 2 ? "CompressedSnappy" : 
+                x == 3 ? "EncryptedGCM" :
+                "Unk_" + v
+            ) : false)
+        ).filter((a) => !!a) as InformPacketFlag[];
+    if (flags.find((a) => a.startsWith("Unk_"))) console.warn("Unknown flags found, decryption/decompresssion may fail! " + flags)
+
+    const pHeader: InformPacketHeader = {
+        rawPacket: packet,
+        rawHeader: header,
+        payload,
+        fields: {
+            version: header.readUint32BE(4),
+            mac: [...packet.subarray(8,14)].map((a) => a.toString(16).padStart(2,'0')).join(":"),
+            encryptionMethod:
+                flags.includes("EncryptedGCM") ? "AES-GCM" :
+                flags.includes("Encrypted") ? "AES-CBC" :
+                "none",
+            compressionMethod: 
+                flags.includes("CompressedSnappy") ? "Snappy" :
+                flags.includes("Compressed") ? "zlib" :
+                "none",
+            iv: packet.subarray(16,32),
+            dataType: header.readUint32BE(32),
+            flags,
+            payloadLength,
+        }
+    };
+    // check version is 0
+    if (pHeader.fields.version !== 0) console.warn("Version was expected to be '0', was " +  pHeader.fields.version)
+        
+    
+    return pHeader;
+}
+
+async function decryptPacket(header: InformPacketHeader, key: webcrypto.BufferSource) {
+    let { encryptionMethod, iv } = header.fields; 
+    if (encryptionMethod == "none") return new Uint8Array(header.payload.buffer);
+
+    var ck =  await subtle.importKey('raw', key, encryptionMethod, true, ['decrypt'])
+    return new Uint8Array(await crypto.subtle.decrypt({name: encryptionMethod, iv, additionalData: header.rawHeader}, ck, header.payload))
+}
+
+
+function decompressPacket(method: InformPacketHeader['fields']['compressionMethod'], compressed: Uint8Array) {
+    if (method == "zlib") return inflateZlib(compressed)
+    else if (method == "Snappy") return unSnappy(compressed)
+    else if (method == "none") return compressed;
+    else throw new Error("Unknown compression method " + method)
+}
+  
+export async function parseInformPacket(packet: Buffer,keyProvider: (mac: string) => Promise<string | undefined> | (string | undefined)) {
+    let head: InformPacketHeader['fields'];
+    let keyUsed: string;
+    let data, error;
+    try {
+        let header = readInformHeader(packet);
+        head = header.fields;
+        
+        let key = await keyProvider(head.mac) || UBNT_DEFAULT_KEY;
+        keyUsed = key;
+        if (key == UBNT_DEFAULT_KEY) keyUsed = "(default)";
+
+        let decrypted = await decryptPacket(header, Buffer.from(key, "hex"));
+        let decompressed = Buffer.from(decompressPacket(head.compressionMethod,decrypted))
+        data = JSON.parse(decompressed.toString())
+    } catch(e) {
+        error = e
+    }
+    return { head, data, error, keyUsed }
+}
